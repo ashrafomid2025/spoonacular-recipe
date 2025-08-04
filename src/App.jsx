704d6ae71bc77574fc3cc41ff3aafae9d43962ae
ashrafomid2025 <omidmashraf@gmail.com>
2025-08-04 21:03:31 +0430

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element=<Home /> />
+          <Route path="/about" element=<About /> />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
